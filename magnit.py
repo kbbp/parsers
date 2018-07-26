c52bf8d73b9b440b6052c170972aa18e18052eba
kbbp <kbbp@users.noreply.github.com>
2018-07-26 20:46:17 +0300

    New parser for Magnit shops

---
+++ magnit.py
@@ -0,0 +1,38 @@
+# add cookies to requests in the next version
+
+from bs4 import BeautifulSoup
+import urllib.request
+import json
+import time
+import re
+
+magnit_url_lenobl = "https://magnitcosmetic.ru/local/ajax/get_city.php?region_id=1766"
+magnit_url_spb = "https://magnitcosmetic.ru/local/ajax/get_city.php?region_id=1722"
+city_url = "https://magnitcosmetic.ru/shops/?chcity="
+magnit_ids_lenobl = []
+magnit_ids_spb = []
+
+lenobl_ids = urllib.request.urlopen(magnit_url_lenobl).read()
+time.sleep(2)
+spb_ids = urllib.request.urlopen(magnit_url_spb).read()
+
+magnit_json_lenobl = json.loads(lenobl_ids)
+magnit_json_spb = json.loads(spb_ids)
+
+for id in magnit_json_lenobl:
+	magnit_ids_lenobl.append(city_url + id.get('id'))
+
+for id in magnit_json_spb:
+	magnit_ids_spb.append(city_url + id.get('id'))
+
+def shop_counter(urls):
+	shops = 0
+	for url in urls:
+		html = urllib.request.urlopen(url).read()
+		soup = BeautifulSoup(html, 'html.parser')
+		shops += len(soup.find_all('div','shops__item')
+		time.sleep(2)
+	return shops
+
+print("Магнит в области ", shop_counter(magnit_ids_spb))
+print("Магнит в городе ", shop_counter(magnit_ids_lenobl))
--- shopc.py
+++ shopc.py
@@ -10,9 +10,11 @@ riv_oblast = 0
 riv_url = "http://www.rivegauche.ru/shops/cities/sankt-peterburg"
 riv_contents = urllib.request.urlopen(riv_url).read()
 riv_soup = BeautifulSoup(riv_contents, 'html.parser')
+
 for shop in riv_soup.find_all('div','shops_rivegauche'):
 	riv_shops.append(shop)
 	riv_shops_count += 1
+	
 for shop in riv_shops:
 	if (shop.find('span','subway') and not shop.find(text=re.compile('Магазин закрыт'))):
 		riv_city += 1
@@ -20,6 +22,7 @@ for shop in riv_shops:
 		riv_oblast += 1
 	if (shop.find(text=re.compile('закрыт'))):
 		riv_closed_shops_count += 1
+		
 print("Рив Гош в городе ", riv_city)
 print("Рив Гош в области ", riv_oblast)
 print("Рив Гош всего ", riv_oblast + riv_city)
@@ -35,9 +38,11 @@ pod_oblast = 0
 pod_url = "https://www.podrygka.ru/shoplist/?set_filter=y&arrShopsSmartFilter_68=450215437"
 pod_contents = urllib.request.urlopen(pod_url).read()
 pod_soup = BeautifulSoup(pod_contents, 'html.parser')
+
 for shop in pod_soup.find_all('div','shops-list-item'):
 	pod_shops.append(shop)
 	pod_shops_count += 1
+	
 for shop in pod_shops:
 	if (shop.find(text=re.compile('Санкт-Петербург,'))):
 		pod_city += 1
@@ -45,6 +50,7 @@ for shop in pod_shops:
 		pod_oblast += 1
 	if (shop.find(text=re.compile('закрыт'))):
 		pod_closed_shops_count += 1
+		
 print("Подружек в городе ", pod_city)
 print("Подружек в области ", pod_oblast)
 print("Подружек всего ", pod_oblast + pod_city)
